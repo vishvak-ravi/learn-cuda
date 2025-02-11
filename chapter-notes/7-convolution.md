6c55fba32329a4d0bbd81e501e998dab548aefb7
VIshvak Ravichandran <vishravi03@gmail.com>
2025-02-11 05:04:15 +0000

    basic + const mem  conv impl

---
+++ chapter-notes/7-convolution.md
@@ -0,0 +1,14 @@
+# Convolution
+## Basics
+- you know what conv is
+- ```__constant__``` keyword to declare constant mem (global across SMs)
+    - ```cudaMemcpyToSymbol(dest, src, amt)``` to initialize without any pointers
+- recall main hope is to maximize ops/glob-mem-access
+## algos
+- tiled
+    - inputTile > outputTile size so tiling can launch some threads to load for a tile but not compute (block size == inputTile size) or launch some threads to doubly load and all compute (blockSize == outputTile size)
+- insight: this method causes LOTS of overlap between input tiles of varying threads
+    - these are likely to reside in L2 cache anyway so each thread may simply store in shared memory input tile corresponding to sie of output tile
+    - this implementation requires careful handling by retrieving from N when not in bounds of N_s
+## exercises
+8. check conv.cu for this
--- programs/convolution/conv.cu
+++ programs/convolution/conv.cu
@@ -2,9 +2,14 @@
 #include <opencv2/opencv.hpp>
 #include <cuda_runtime.h>
 #include <string.h>
-#include "misc.cuh"
 
-#define CHANNELS 3
+#define FILTER_RADIUS 2
+__constant__ float F[2 * FILTER_RADIUS + 1][2 * FILTER_RADIUS + 1];
+
+enum Method {
+    SIMPLE,
+    GLOBAL
+};
 
 unsigned char* imageToArray(const char* filePath, int* width, int* height, int* channels) {
     printf("started image conversion\n");
@@ -15,7 +20,7 @@ unsigned char* imageToArray(const char* filePath, int* width, int* height, int* 
     }
     *height = img.rows;
     *width = img.cols;
-    *channels = img.channels();
+    *channels = img.channels(); // should be 3 for ALL color images
     printf("channels %d\n", *channels);
 
     int size = (*height) * (*width) * (*channels) * sizeof(unsigned char);
@@ -25,20 +30,20 @@ unsigned char* imageToArray(const char* filePath, int* width, int* height, int* 
     return imgArr;
 }
 
-void saveImageFromData(const char* filePath, unsigned char* dataArray, int* width, int* height, int* channels) {
-    if (dataArray == NULL || (*channels != 1 && *channels != 3)) {
+void saveImageFromData(const char* filePath, unsigned char* dataArray, int width, int height, int channels) {
+    if (dataArray == NULL || (channels != 1 && channels != 3)) {
         fprintf(stderr, "Invalid input data or unsupported channel count (only 1 or 3 channels supported)\n");
         return;
     }
 
     // Create a cv::Mat based on the channel count
     cv::Mat image;
-    if (*channels == 3) {
+    if (channels == 3) {
         // RGB image (3 channels)
-        image = cv::Mat(*height, *width, CV_8UC3, dataArray);
-    } else if (*channels == 1) {
+        image = cv::Mat(height, width, CV_8UC3, dataArray);
+    } else if (channels == 1) {
         // Grayscale image (1 channel)
-        image = cv::Mat(*height, *width, CV_8UC1, dataArray);
+        image = cv::Mat(height, width, CV_8UC1, dataArray);
     }
 
     // Save the image using OpenCV's imwrite function
@@ -48,50 +53,135 @@ void saveImageFromData(const char* filePath, unsigned char* dataArray, int* widt
 }
 
 __global__
-void rgbToGrayscaleKernel(unsigned char* P_in, unsigned char* P_out, int height, int width) {
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
+void crossCorrelationKernel(unsigned char* N, float* f, int* P, int height, int width, int channels, int r) {
+    // poor memory coalescing 
+
+    //assumes even number of filter_size
+    int chan = blockIdx.z * blockDim.z;
     int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
 
-    if (row < height && col < width) {
-        int grayOffset = row * width + col;
-        int rgbOffset = CHANNELS * grayOffset;
-        unsigned char r = P_in[rgbOffset];
-        unsigned char g = P_in[rgbOffset + 1];
-        unsigned char b = P_in[rgbOffset + 2];
-        P_out[grayOffset] = 0.21f*r + 0.71f*g + 0.07f*b;
+    int Pval = 0;
+
+    for (int fRow = 0; fRow < 2*r + 1; fRow++) {
+        for (int fCol = 0; fCol < 2*r + 1; fCol++) {
+            int inCol = col - r + fCol;
+            int inRow = row - r + fRow;
+            if (inCol >= 0 && inCol < width && inRow >= 0 && inRow < height) {
+                Pval += N[inRow * (channels * width) + inCol * (channels) + chan] * f[fCol * (2*r + 1) + fRow];
+            }
+        }
     }
-}
-
-void rgbToGrayscale(unsigned char* P_in_h, unsigned char* P_out_h, int height, int width) {
-    unsigned char *P_in_d, *P_out_d;
-    int size = height * width * 3;
-    cudaMalloc((void **) &P_in_d, size);
-    cudaMalloc((void **) &P_out_d, size);
-
-    cudaMemcpy(P_in_d, P_in_h, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(P_out_d, P_out_h, size, cudaMemcpyHostToDevice);
-
-    int threadCount = 12;
-
-    dim3 gridDim(ceil(1.0 * width / threadCount), ceil(1.0 * height / threadCount));
-    dim3 blockDim(threadCount, threadCount, 1);
-
-    rgbToGrayscaleKernel<<<gridDim, blockDim>>>(P_in_d, P_out_d, height, width);
-
-    cudaMemcpy(P_out_h, P_out_d, size, cudaMemcpyDeviceToHost);
-
-    cudaFree(P_in_d);
-    cudaFree(P_out_d);
+    P[row * (channels * width) + col * (channels) + chan] = Pval;
 }
 
 __global__
-void crossCorrelationKernel(unsigned char* P_in_h, float* filter, unsigned char* P_out_h, int height, int width, int filter_half_width) {
+void constMemCrossCorrelationKernel(unsigned char* N, int* P, int height, int width, int channels, int r) {
+    // poor memory coalescing 
+
     //assumes even number of filter_size
+    int chan = blockIdx.z * blockDim.z;
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
-    for (int i = 0; i < filter_size; i++) {
 
+    int Pval = 0;
+
+    for (int fRow = 0; fRow < 2*r + 1; fRow++) {
+        for (int fCol = 0; fCol < 2*r + 1; fCol++) {
+            int inCol = col - r + fCol;
+            int inRow = row - r + fRow;
+            if (inCol >= 0 && inCol < width && inRow >= 0 && inRow < height) {
+                Pval += N[inRow * (channels * width) + inCol * (channels) + chan] * F[fRow][fCol];
+            }
+        }
     }
+    P[row * (channels * width) + col * (channels) + chan] = Pval;
+}
+
+void crossCorrelation(unsigned char* N, float* k, unsigned char* P, int height, int width, int channels, int r, Method method) {
+    // assumes filter is single channel and applies independently across input channels
+    float maxFilterValue = 0.0f;
+    int filterSize = (2*r + 1) * (2*r + 1);
+    for (int i = 0; i < filterSize; i++) {
+        if (k[i] > maxFilterValue) {
+            maxFilterValue = k[i];
+        }
+    }
+
+    if (maxFilterValue > 1.0f) {
+        for (int i = 0; i < filterSize; i++) {
+            k[i] /= maxFilterValue;
+        }
+    }
+
+    unsigned char* N_d;
+    int* P_d;
+    float* k_d; // ensure filter has max value of 1
+    cudaMalloc((void **) &N_d, height * width * channels * sizeof(unsigned char));
+    cudaMalloc((void **) &P_d, height * width * channels * sizeof(int)); // ensure 
+    cudaMalloc((void **) &k_d, r * r * sizeof(float));
+
+    cudaMemcpy(N_d, N, height * width * channels * sizeof(unsigned char), cudaMemcpyHostToDevice);
+    cudaMemcpy(k_d, k, r * r * sizeof(float), cudaMemcpyHostToDevice);
+
+    cudaEvent_t start, stop;
+    float elapsedTime;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    switch (method) {
+        case SIMPLE: {
+            int threadCount = 16;
+            dim3 gridDim(ceil(1.0 * height / threadCount), ceil(1.0 * width / threadCount), channels);
+            dim3 blockDim(threadCount, threadCount, 1); //
+
+            cudaEventRecord(start, 0);
+            crossCorrelationKernel<<<gridDim, blockDim>>>(N_d, k_d, P_d, height, width, channels, r);
+            cudaEventRecord(stop, 0);
+            cudaEventSynchronize(stop);
+            cudaEventElapsedTime(&elapsedTime, start, stop);
+            printf("SIMPLE kernel execution time: %f ms\n", elapsedTime);
+            break;
+        }
+        case GLOBAL: {
+            cudaMemcpyToSymbol(F, k, (2 * r + 1) * (2 * r + 1) * sizeof(float));
+            int threadCount = 16;
+            dim3 gridDim(ceil(1.0 * height / threadCount), ceil(1.0 * width / threadCount), channels);
+            dim3 blockDim(threadCount, threadCount, 1);
+            
+            cudaEventRecord(start, 0);
+            constMemCrossCorrelationKernel<<<gridDim, blockDim>>>(N_d, P_d, height, width, channels, r);
+            cudaEventRecord(stop, 0);
+            cudaEventSynchronize(stop);
+            cudaEventElapsedTime(&elapsedTime, start, stop);
+            printf("GLOBAL kernel execution time: %f ms\n", elapsedTime);
+            break;
+        }
+    }
+
+    int* temp_P = new int[height * width * channels];
+    cudaMemcpy(temp_P, P_d, height * width * channels * sizeof(int), cudaMemcpyDeviceToHost);
+
+    int maxVal = temp_P[0];
+    int minVal = temp_P[0];
+    for (int i = 1; i < height * width * channels; i++) {
+        if (temp_P[i] > maxVal) {
+            maxVal = temp_P[i];
+        }
+        if (temp_P[i] < minVal) {
+            minVal = temp_P[i];
+        }
+    }
+    
+    for (int i = 0; i < height * width * channels; i++) {
+        P[i] = (temp_P[i] - minVal) * 255 / (maxVal - minVal);
+    }
+    
+    delete[] temp_P;
+
+    cudaFree(N_d);
+    cudaFree(P_d);
+    cudaFree(k_d);
 }
 
 int main(int argc, char** argv) {
@@ -101,16 +191,25 @@ int main(int argc, char** argv) {
     char* sourcePath = "fake_img.jpg";
     unsigned char* imgArr = imageToArray("fake_img.jpg", &width, &height, &channels);
     printf("height: %d, width: %d\n", height, width);
-    unsigned char* transformedImgArr= new unsigned char[height * width];
+    unsigned char* transformedImgArr= new unsigned char[height * width * channels];
 
-    rgbToGrayscale(imgArr, transformedImgArr, height, width);
+    // create filter
+    int filter_radius = FILTER_RADIUS;
+    int filter_size = 2 * filter_radius + 1;
+    float* filter = new float[filter_size * filter_size];
+    for (int i = 0; i < filter_size * filter_size; i++) {    
+        filter[i] = 1;
+    }
+
+    for (Method method = Method::SIMPLE; method <= Method::GLOBAL; method = static_cast<Method>(method + 1)) {
+        crossCorrelation(imgArr, filter, transformedImgArr, height, width, channels, filter_radius, method);
+    }
 
     char* savePath = new char[256];
     sprintf(savePath, "transformed_%s", sourcePath);
-    int grayChannels = 1;
-    int* grayChannelsPtr = &grayChannels;
-    saveImageFromData(savePath, transformedImgArr, &width, &height, grayChannelsPtr);
+    saveImageFromData(savePath, transformedImgArr, width, height, 3);
 
     delete[] imgArr;
     delete[] transformedImgArr;
+    delete[] filter;
 }
